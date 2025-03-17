97dbec6dde10f7a8e33712a159f03051a6bec7cd
iwolfking <thegreatiwk@gmail.com>
2025-03-17 11:41:40 -0500

    Fix #45, raw chromatic iron can't be crushed with Occultism

--- kubejs/server_scripts/vh_compat/occultism/crushing.js
+++ kubejs/server_scripts/vh_compat/occultism/crushing.js
@@ -10,4 +10,16 @@ onEvent("recipes", event => {
         },
         "crushing_time": 200
       })
+
+      event.custom({
+        "type": "occultism:crushing",
+        "ingredient": {
+          "item": "the_vault:raw_chromatic_iron"
+        },
+        "result": {
+          "item": "the_vault:chromatic_iron_dust",
+          "count": 2
+        },
+        "crushing_time": 200
+      })
 })
