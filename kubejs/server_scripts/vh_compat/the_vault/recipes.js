f6c985b1f7797d1a33f68e5dbe0decd8b1d881c5
iwolfking <thegreatiwk@gmail.com>
2025-05-12 10:50:42 -0500

    Remove etching smelt recipe

--- kubejs/server_scripts/vh_compat/the_vault/recipes.js
+++ kubejs/server_scripts/vh_compat/the_vault/recipes.js
@@ -5,6 +5,7 @@ onEvent("recipes", event => {
     event.remove({id: 'the_vault:infused_eternal_soul'})
     event.remove({output: 'the_vault:coin_pouch'})
     event.remove({id: 'the_vault:echoing_ingot'})
+    event.remove({id: 'the_vault:smelt_etching'})
 
     event.shaped(Item.of('the_vault:crystal_budding'),
         [
