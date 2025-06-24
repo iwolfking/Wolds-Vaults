8a75557a567c5344331ee079653b6befe86cb136
iwolfking <thegreatiwk@gmail.com>
2025-06-24 10:46:34 -0500

    Hide all dungeons mobs items from JEI

--- kubejs/client_scripts/vh_compat/general/jei_hide.js
+++ kubejs/client_scripts/vh_compat/general/jei_hide.js
@@ -97,6 +97,7 @@ onEvent('jei.hide.items', event => {
     event.hide(/quark:.*_sign/)
     event.hide(/tropicraft:.*_sign/)
     event.hide(/twigs:.*_sign/)
+    event.hide(/dungeons_mobs:.*/)
 })
 
 
