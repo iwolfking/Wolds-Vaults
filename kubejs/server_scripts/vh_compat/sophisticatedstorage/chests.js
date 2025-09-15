7723b0aac06d56df961ade3d47b67840317299b4
iwolfking <thegreatiwk@gmail.com>
2025-09-15 09:14:17 -0500

    Add Sophisticated Storage versions of Unique Crate and the Vault Barrels

--- kubejs/server_scripts/vh_compat/sophisticatedstorage/chests.js
+++ kubejs/server_scripts/vh_compat/sophisticatedstorage/chests.js
@@ -10,6 +10,19 @@ onEvent("recipes", event => {
             result: result
         })
     }
+
+    event.shaped(Item.of('sophisticatedstorage:unique_crate'),
+        [
+            'ITI',
+            'XCX',
+            'IEI'
+        ], {
+        C: 'the_vault:unique_crate_scroll',
+        X: 'the_vault:chromatic_gold_block',
+        I: 'woldsvaults:chroma_core',
+        T: 'the_vault:gem_pog',
+        E: 'woldsvaults:enigma_egg'
+    }).id('sophisticatedstorage:unique_crate')
   
     event.shaped(Item.of('sophisticatedstorage:treasure_chest'),
         [
@@ -17,11 +30,11 @@ onEvent("recipes", event => {
             'XCX',
             'ITI'
         ], {
-        C: 'the_vault:treasure_chest',
+        C: 'the_vault:treasure_chest_scroll',
         X: 'the_vault:vault_diamond_block',
         I: 'the_vault:pog_prism',
         T: 'the_vault:extraordinary_larimar'
-    })
+    }).id('sophisticatedstorage:treasure_chest')
 
     event.shaped(Item.of('sophisticatedstorage:wooden_chest'),
         [
@@ -29,10 +42,22 @@ onEvent("recipes", event => {
             'XCX',
             'IXI'
         ], {
-        C: 'the_vault:wooden_chest',
+        C: 'the_vault:wooden_chest_scroll',
         X: 'the_vault:vault_diamond',
-        I: 'the_vault:wooden_planks'
-    })
+        I: 'the_vault:driftwood_planks'
+    }).id('sophisticatedstorage:wooden_chest')
+
+    event.shaped(Item.of('sophisticatedstorage:wooden_barrel'),
+        [
+            'IXI',
+            'PCP',
+            'IXI'
+        ], {
+        C: 'the_vault:wooden_chest_scroll',
+        X: 'the_vault:vault_diamond',
+        I: 'woldsvaults:infused_driftwood',
+        P: 'the_vault:chromatic_steel_ingot'
+    }).id('sophisticatedstorage:wooden_barrel')
 
     event.shaped(Item.of('sophisticatedstorage:hardened_chest'),
         [
@@ -40,10 +65,10 @@ onEvent("recipes", event => {
             'XCX',
             'IXI'
         ], {
-        C: 'the_vault:hardened_chest',
-        X: 'the_vault:vault_stone',
+        C: 'the_vault:hardened_chest_scroll',
+        X: 'the_vault:vault_diamond',
         I: 'the_vault:chromatic_iron_ingot'
-    })
+    }).id('sophisticatedstorage:hardened_chest')
 
     event.shaped(Item.of('sophisticatedstorage:flesh_chest'),
         [
@@ -51,10 +76,10 @@ onEvent("recipes", event => {
             'XCX',
             'IXI'
         ], {
-        C: 'the_vault:flesh_chest',
+        C: 'the_vault:flesh_chest_scroll',
         X: 'the_vault:gem_painite',
         I: 'the_vault:vault_meat_block'
-    })
+    }).id('sophisticatedstorage:flesh_chest')
 
     event.shaped(Item.of('sophisticatedstorage:enigma_chest'),
         [
@@ -62,10 +87,10 @@ onEvent("recipes", event => {
             'XCX',
             'IXI'
         ], {
-        C: 'the_vault:enigma_chest',
+        C: 'the_vault:enigma_chest_scroll',
         X: 'the_vault:black_chromatic_steel_ingot',
-        I: 'the_vault:perfect_wutodie'
-    })
+        I: 'the_vault:wutodic_mass'
+    }).id('sophisticatedstorage:enigma_chest')
 
 
     event.shaped(Item.of('sophisticatedstorage:altar_chest'),
@@ -74,10 +99,10 @@ onEvent("recipes", event => {
             'XCX',
             'IXI'
         ], {
-        C: 'the_vault:altar_chest',
+        C: 'the_vault:altar_chest_scroll',
         X: 'the_vault:black_chromatic_steel_ingot',
-        I: 'the_vault:perfect_larimar'
-    })
+        I: 'the_vault:extraordinary_larimar'
+    }).id('sophisticatedstorage:altar_chest')
 
     event.shaped(Item.of('sophisticatedstorage:living_chest'),
         [
@@ -85,10 +110,22 @@ onEvent("recipes", event => {
             'XCX',
             'IXI'
         ], {
-        C: 'the_vault:living_chest',
-        X: 'the_vault:mossy_bone_block',
-        I: 'the_vault:perfect_alexandrite'
-    })
+        C: 'the_vault:living_chest_scroll',
+        X: 'woldsvaults:chroma_core',
+        I: 'the_vault:vault_meat_block'
+    }).id('sophisticatedstorage:living_chest')
+
+    event.shaped(Item.of('sophisticatedstorage:living_barrel'),
+        [
+            'IXI',
+            'PCP',
+            'IXI'
+        ], {
+        C: 'the_vault:living_chest_scroll',
+        X: 'woldsvaults:chroma_core',
+        I: 'the_vault:vault_meat_block',
+        P: 'the_vault:black_chromatic_steel_ingot'
+    }).id('sophisticatedstorage:living_barrel')
 
     event.shaped(Item.of('sophisticatedstorage:ornate_chest'),
         [
@@ -96,10 +133,22 @@ onEvent("recipes", event => {
             'XCX',
             'IXI'
         ], {
-        C: 'the_vault:ornate_chest',
-        X: 'the_vault:ornate_block',
-        I: 'the_vault:perfect_painite'
-    })
+        C: 'the_vault:ornate_chest_scroll',
+        X: 'woldsvaults:chroma_core',
+        I: 'the_vault:vaulterite_ingot'
+    }).id('sophisticatedstorage:ornate_chest')
+
+    event.shaped(Item.of('sophisticatedstorage:ornate_barrel'),
+        [
+            'IXI',
+            'PCP',
+            'IXI'
+        ], {
+        C: 'the_vault:ornate_chest_scroll',
+        X: 'woldsvaults:chroma_core',
+        I: 'the_vault:vaulterite_ingot',
+        P: 'the_vault:black_chromatic_steel_ingot'
+    }).id('sophisticatedstorage:ornate_barrel')
 
     event.shaped(Item.of('sophisticatedstorage:gilded_chest'),
         [
@@ -107,10 +156,22 @@ onEvent("recipes", event => {
             'XCX',
             'IXI'
         ], {
-        C: 'the_vault:gilded_chest',
-        X: 'the_vault:gilded_block',
+        C: 'the_vault:gilded_chest_scroll',
+        X: 'woldsvaults:chroma_core',
         I: 'the_vault:chromatic_gold_ingot'
-    })
+    }).id('sophisticatedstorage:gilded_chest')
+
+    event.shaped(Item.of('sophisticatedstorage:gilded_barrel'),
+        [
+            'IXI',
+            'PCP',
+            'IXI'
+        ], {
+        C: 'the_vault:gilded_chest_scroll',
+        X: 'woldsvaults:chroma_core',
+        I: 'the_vault:chromatic_gold_ingot',
+        P: 'the_vault:black_chromatic_steel_ingot'
+    }).id('sophisticatedstorage:gilded_barrel')
 
     strongboxUpgrade(Item.of('sophisticatedstorage:ornate_strongbox'),
         {
