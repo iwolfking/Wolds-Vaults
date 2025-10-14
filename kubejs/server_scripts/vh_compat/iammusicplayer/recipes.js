0c8da2ebf5de01947f9a2cbb68edf84f837c0c4d
iwolfking <thegreatiwk@gmail.com>
2025-10-13 21:26:42 -0500

    Add recipe for Parabolic Antenna

---
+++ kubejs/server_scripts/vh_compat/iammusicplayer/recipes.js
@@ -0,0 +1,16 @@
+
+onEvent("recipes", event => {
+
+    event.shaped(Item.of('iammusicplayer:parabolic_antenna'),
+        [
+            'A  ',
+            ' AI',
+            ' II'
+        ], {
+        A: 'iammusicplayer:antenna',
+        I: 'minecraft:iron_block'
+    }).id('iammusicplayer:parabolic_antenna')
+
+
+
+})
