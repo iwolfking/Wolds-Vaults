fd95a55072c50c24c4888dd1a6c0dbc766f537f9
JGrade2005 <jgrade942@gmail.com>
2026-03-30 01:29:03 -0400

    Finish Draconic Evolution compat with all recipes

--- kubejs/server_scripts/vh_compat/draconicevolution/recipes.js
+++ kubejs/server_scripts/vh_compat/draconicevolution/recipes.js
@@ -2,9 +2,24 @@ let removedOutputsDE = [
     "draconicevolution:draconium_core",
     "draconicevolution:wyvern_core",
     "draconicevolution:module_core",
+    "draconicevolution:disenchanter",
+    "draconicevolution:particle_generator",
+    "draconicevolution:energy_core_stabilizer",
+    "draconicevolution:grinder",
+    "draconicevolution:wyvern_energy_core",
+    "draconicevolution:small_chaos_frag",
+    "draconicevolution:draconium_chest",
+    "draconicevolution:celestial_manipulator",
+    "draconicevolution:dislocation_inhibitor",
+    "draconicevolution:crafting_core",
+    "draconicevolution:basic_crafting_injector",
+    "draconicevolution:reactor_prt_focus_ring",
+    "draconicevolution:reactor_prt_stab_frame",
+
     /draconicadditions:.*_harness/,
-    /draconicadditions:.chaos_*/,
+    /draconicadditions:.*chaos_*/,
     /draconicadditions:.*_necklace/,
+    /draconicevolution:.*_gate/,
     /draconicevolution:.*dislocator/,
     /draconicevolution:.*_sword/,
     /draconicevolution:.*_staff/,
@@ -27,7 +42,7 @@ let removedOutputsDE = [
     /draconicevolution:.*_filter_module/,
     /draconicevolution:.*_aoe_module/,
     /draconicevolution:.*_speed_module/,
-    /draconicevolution:.*relay_crystal/
+    /draconicevolution:.(basic|wyvern)_relay_crystal/
 ];
 onEvent("recipes", event => {
     removedOutputsDE.forEach(id => {
@@ -44,6 +59,8 @@ onEvent("recipes", event => {
             T: 'the_vault:black_chromatic_steel_ingot',
             R: 'woldsvaults:pog_prism',
         })
+
+
     event.shaped(Item.of('draconicevolution:wyvern_core'),
         [
             'STS',
@@ -54,6 +71,8 @@ onEvent("recipes", event => {
             T: 'draconicevolution:draconium_core',
             R: 'woldsvaults:extraordinary_pog_prism',
         })
+
+
     event.shaped(Item.of('draconicevolution:generator'),
         [
             'STS',
@@ -65,6 +84,8 @@ onEvent("recipes", event => {
             R: 'draconicevolution:draconium_core',
             F: 'woldsvaults:chromatic_iron_vault_infuser'
         })
+
+
     event.shaped(Item.of('draconicevolution:grinder'),
         [
             'SDS',
@@ -77,47 +98,179 @@ onEvent("recipes", event => {
             W: 'draconicevolution:wyvern_energy_core',
             H: '#forge:heads'
         })
+
+
     event.shaped(Item.of('draconicevolution:basic_relay_crystal'),
         [
             ' D ',
             'DWD',
             'HHH',
         ], {
-            D: 'the_vault:extraordinary_echo_gem'
-            W: 'draconicevolution:wyvern_energy_core'
+            D: 'the_vault:extraordinary_echo_gem',
+            W: 'draconicevolution:wyvern_energy_core',
             H: 'draconicevolution:draconium_block'
         })
+
+
     event.shaped(Item.of('draconicevolution:wyvern_relay_crystal'),
         [
             'HDH',
             'DWD',
             'HDH',
         ], {
-            D: 'draconicevolution:basic_relay_crystal'
-            W: 'draconicevolution:wyvern_core'
+            D: 'draconicevolution:basic_relay_crystal',
+            W: 'draconicevolution:wyvern_core',
             H: 'draconicevolution:draconium_block'
         })
+
+
     event.shaped(Item.of('draconicevolution:wyvern_energy_core'),
         [
             'HDH',
             'DWD',
             'HDH',
         ], {
-            D: 'draconicevolution:draconium_core'
-            W: 'woldsvaults:prismatic_fiber_block'
+            D: 'draconicevolution:draconium_core',
+            W: 'woldsvaults:prismatic_fiber_block',
             H: '#forge:ingots/draconium'
         })
+
+
     event.shaped(Item.of('draconicevolution:module_core'),
         [
             'IRI',
             'GWG',
             'IRI',
         ], {
-            I: 'the_vault:chromatic_iron_block'
-            W: '#forge:ingots/draconium'
-            G: 'woldsvaults:chromatic_gold_ingot'
+            I: 'the_vault:chromatic_iron_block',
+            W: '#forge:ingots/draconium',
+            G: 'woldsvaults:chromatic_gold_ingot',
             R: 'minecraft:redstone_block'
         })
 
+    event.shaped(Item.of('draconicevolution:disenchanter'),
+        [
+            'PCP',
+            'VEV',
+            'BBB',
+        ], {
+            P: 'the_vault:echo_pog',
+            C: 'draconicevolution:draconium_core',
+            V: 'minecraft:enchanted_book',
+            E: 'minecraft:enchanting_table',
+            B: 'woldsvaults:pog_block'
+        })
+
+    event.shaped(Item.of('draconicevolution:particle_generator'),
+        [
+            'IRI',
+            'GWG',
+            'IRI',
+        ], {
+            I: 'the_vault:chromatic_iron_block',
+            W: 'draconicevolution:draconium_core',
+            G: 'woldsvaults:chromatic_gold_ingot',
+            R: 'botania:blaze_block'
+        })
+
+    event.shaped(Item.of('draconicevolution:energy_core_stabilizer'),
+        [
+            'I I',
+            ' W ',
+            'I I',
+        ], {
+            I: 'the_vault:extraordinary_larimar',
+            W: 'draconicevolution:particle_generator'
+        })
+    event.shaped(Item.of('draconicevolution:small_chaos_frag'),
+        [
+            'I I',
+            ' W ',
+            'I I',
+        ], {
+            I: 'the_vault:extraordinary_larimar',
+            W: 'the_vault:perfect_echo_gem'
+        })
+
+
+    event.shaped(Item.of('draconicevolution:crafting_core'),
+        [
+            'CCC',
+            'CWC',
+            'CCC',
+        ], {
+            C: 'compressium:chromatic_steel_block_2',
+            W: 'draconicevolution:wyvern_core'
+        })
+
+
+    event.shaped(Item.of('draconicevolution:basic_crafting_injector'),
+        [
+            'CWC',
+            'CGC',
+            'BWB',
+        ], {
+            C: 'compressium:chromatic_steel_block_1',
+            W: 'draconicevolution:wyvern_core',
+            G: 'woldsvaults:chromatic_gold_block',
+            B: 'the_vault:black_chromatic_steel_block'
+        })
+
+    event.shaped(Item.of('draconicevolution:reactor_prt_focus_ring'),
+        [
+            'CCC',
+            'CWC',
+            'CCC',
+        ], {
+            C: 'the_vault:black_chromatic_steel_ingot',
+            W: 'draconicevolution:chaotic_energy_core'
+        })
+
+    event.shaped(Item.of('draconicevolution:reactor_prt_stab_frame'),
+        [
+            'CCC',
+            'WDD',
+            'CCC',
+        ], {
+            C: 'the_vault:black_chromatic_steel_ingot',
+            W: 'draconicevolution:chaotic_energy_core',
+            D: 'draconicevolution:awakened_draconium_ingot'
+        })
+
+    event.shaped(Item.of('draconicevolution:flux_gate'),
+        [
+            'CPC',
+            'RWR',
+            'CMC',
+        ], {
+            C: 'the_vault:chromatic_iron_ingot',
+            P: 'draconicevolution:potentiometer',
+            R: 'minecraft:redstone_block',
+            W: 'draconicevolution:draconium_core',
+            M: 'minecraft:comparator'
+        })
+
+    event.shaped(Item.of('draconicevolution:fluid_gate'),
+        [
+            'CPC',
+            'RWR',
+            'CMC',
+        ], {
+            C: 'the_vault:chromatic_iron_ingot',
+            P: 'draconicevolution:potentiometer',
+            R: 'minecraft:bucket',
+            W: 'draconicevolution:draconium_core',
+            M: 'minecraft:comparator'
+        })
+
+    event.shaped(Item.of('draconicevolution:dragon_heart'),
+        [
+            'CCC',
+            'CWC',
+            'CCC',
+        ], {
+            C: 'draconicevolution:chaos_shard',
+            W: 'draconicevolution:awakened_core'
+        })
 
 })
