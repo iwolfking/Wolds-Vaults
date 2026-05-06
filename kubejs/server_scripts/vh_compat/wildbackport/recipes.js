615b6ceee9c844f207621b316f3fcb22ed6c25fd
iwolfking <thegreatiwk@gmail.com>
2026-05-06 14:34:36 -0500

    Add mud conversion recipes

---
+++ kubejs/server_scripts/vh_compat/wildbackport/recipes.js
@@ -0,0 +1,9 @@
+
+onEvent("recipes", event => {
+    event.shapeless('wildbackport:mud', ['buildscape:mud']).id('woldsvaults:wb_mud_buildscape')
+    event.shapeless('buildscape:mud', ['wildbackport:mud']).id('woldsvaults:buildscape_mud_wb')
+
+
+
+
+})
